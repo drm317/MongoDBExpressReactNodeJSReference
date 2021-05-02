9889798875b407175a7bd8489ae74b54eb7a0f22
Daniel Marlow <drm3107@gmail.com>
2021-05-02 20:02:41 +0100

    Initial commit of Express backend

---
+++ backend/api/restaurants.route.js
@@ -0,0 +1,10 @@
+import express from "express"
+import RestaurantsCtrl from "./restaurants.controller.js"
+
+const router = express.Router()
+
+router.route("/").get(RestaurantsCtrl.apiGetRestaurants)
+router.route("/id/:id").get(RestaurantsCtrl.apiGetRestaurantById)
+router.route("/cuisines").get(RestaurantsCtrl.apiGetRestaurantCuisines)
+
+export default router
