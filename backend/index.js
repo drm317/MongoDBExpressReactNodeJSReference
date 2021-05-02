c755354a02935fdab4425b6f43f60ec25e017628
Daniel Marlow <drm3107@gmail.com>
2021-05-02 20:32:35 +0100

    Added reviewsDAO to index.js

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import app from "./server.js"
 import mongodb from "mongodb"
 import dotenv from "dotenv"
 import RestaurantsDAO from "./dao/restaurantsDAO.js"
+import ReviewsDAO from "./dao/reviewsDAO.js"
 
 dotenv.config()
 const MongoClient = mongodb.MongoClient
@@ -21,6 +22,7 @@ MongoClient.connect(
 })
 .then(async client => {
     await RestaurantsDAO.injectDB(client)
+    await ReviewsDAO.injectDB(client)
     app.listen(port, () => {
       console.log(`listening on port ${port}`)
     })
