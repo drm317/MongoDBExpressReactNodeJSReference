c9eb5204d8dd972acc4e1544b14a8aac18ffb733
Himanshu Suresh Chaudhari <himanshuchaudhari@Himanshus-MacBook-Air.local>
2025-01-17 11:16:52 +0530

    AddingDocker

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5000/api/v1/restaurants",
+  baseURL: "http://localhost:5001/api/v1/restaurants",
   headers: {
     "Content-type": "application/json"
   }
